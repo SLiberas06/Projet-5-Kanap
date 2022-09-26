f4b49bffaa11eb4d0d0c17511a55afef11ef5620
Sabrina Liberas <sabrina.liberas@hotmail.fr>
2022-09-26 18:27:16 +0200

    Ajout panier Local Storage

--- front/js/product.js
+++ front/js/product.js
@@ -40,6 +40,9 @@ async function addDetail(detail){
     //Ajout de la description du produit
     let description = document.querySelector('#description');
         description.textContent = `${detail.description}`;
+    //Ajout de la quantité en stock du produit
+    let quantity = document.querySelector('#quantity');
+        quantity.innerHTML =`${100}`;
     //Boucle d'ajout choix des "options de couleur"
     for (let color of detail.colors){
         let colorChoice = document.createElement("option");
@@ -52,33 +55,83 @@ addCart(detail);
 //---------------------------------Ajout au panier--------------------------------------------
 //Récupération des données sélectionnées et l'envoi au panier
 function addCart(detail){
-//Sélection de id du formulaire
-const colorChoice = document.querySelector("#colors");
-const quantityChoice = document.querySelector("#quantity");
 
-//Selection du bouton "Ajouter au panier"
-const btn_addToCart = document.querySelector('#addToCart');
-// console.log(btn_addToCart);
+    //Sélection de id du formulaire
+    const colorChoice = document.querySelector("#colors");
+    const quantityChoice = document.querySelector("#quantity");
 
-//Ecouter le bouton et ajouter au panier
-btn_addToCart.addEventListener("click",(event)=>{
-   
+    //Selection du bouton "Ajouter au panier"
+    const btn_addToCart = document.querySelector('#addToCart');
+    // console.log(btn_addToCart);
 
-//Intégrer les choix en variables
-let optionColor = colorChoice.value;
-let optionQuantity = quantityChoice.value;
+        //Ecouter le bouton et ajouter au panier
+        btn_addToCart.addEventListener("click",(event)=>{
+        
 
-//Récuperation des valeurs du formulaire
+            //Intégrer les choix en variables
+            let optionColor = colorChoice.value;
+            let optionQuantity = quantityChoice.value;
+            //Si la couleur n'est pas sélectionnée
+                // if(optionColor =! optionColor){
+                //     alert("Merci de sélectionner la couleur souhaitée !");
+                // }else{
+                
+                // };
+            //Si l'utilisateur choisit une quantité supérieur au stock évoqué
+                if(optionQuantity > 100){
+                    alert( "Quantité limitée");
+                }
+            //Sinon , si il ne sélectionne pas de quantité    
+                else if(optionQuantity < 1){
+                    alert("Merci de sélectionner la quantité souhaitée !");
+                };
+            
+            //Récuperation des valeurs du formulaire
+            let optionProduct = { 
+                productName : detail.name,
+                productId: idProduct,
+                choiceColor : optionColor,
+                quantity :optionQuantity,
+                productPrice : detail.price,
+                productDescription : detail.description,
+                productImage : detail.imageUrl,
+                imageAlt : detail.altTxt
+               
+            };
+            //conditions de calcul du prix par rapport à la quantité
+            if(optionQuantity + 1){
+                optionProduct.productPrice = optionProduct.productPrice * optionQuantity;
+            };
 
-let optionProduct = { 
-    productName : detail.name,
-    productId: idProduct,
-    choiceColor : optionColor,
-    quantity :optionQuantity,
-    productPrice : detail.price,
-    productDescription : detail.description,
-    productImage : detail.imageUrl,
-    imageAlt : detail.altTxt
-}
-console.table(optionProduct);
-})};
+            console.log(optionColor);
+//------------------------------------------Local storage-----------------------------------------------------------
+//--------------------------Stocker les valeurs du formulaire dans le storage---------------------------------------
+//déclaration de la variable "produit dans le local storage", puis convertir les données au format JSON en objet JavaScript
+            let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+                //fonction popup
+                // const popupConfirmation = () =>{
+                //     if(window.confirm(`${detail.name} option :${optionColor},${optionQuantity} a bien été ajouté au panier
+                //     Voir le panier OK ou continuer mes achats ANNULER`)){
+                //         window.location.href = "cart.html";
+
+                //     }else{
+                //         window.location.href = "index.html";
+                //     }
+                // }
+                //si il y a des produits enregistrés dans le local storage 
+                // if(productInLocalStorage){
+                //     productInLocalStorage.push(optionProduct);
+                //     localStorage.setItem('product',JSON.stringify(productInLocalStorage));
+                //     console.log(productInLocalStorage);
+                //     popupConfirmation();
+                // }
+                // //si il n'y a pas de produits enregistrés dans le local storage
+                // else{
+                // productInLocalStorage = [];
+                // productInLocalStorage.push(optionProduct);
+                // localStorage.setItem('product',JSON.stringify(productInLocalStorage));
+                // console.log(productInLocalStorage);
+                // popupConfirmation();
+                // }
+        })
+    }; 

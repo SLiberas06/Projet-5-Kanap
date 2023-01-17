f4a25fd8b5048450cf08de4107116519fdebdcf9
Sabrina Liberas <sabrina.liberas@hotmail.fr>
2023-01-17 09:45:14 +0100

    modif deploiement

--- back/app.js
+++ back/app.js
@@ -6,7 +6,7 @@ const productRoutes = require('./routes/product');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://projet-5-kanap-psi.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://projet-5-kanap-psi.vercel.app');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
